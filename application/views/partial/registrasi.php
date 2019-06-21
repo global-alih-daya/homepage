2d30f33ab2566415239a8f165dbf57204b7a0941
ravhi <ravhi.rizaldi@gad.co.id>
2019-06-21 23:34:22 +0700

    fix mixed content of https failure

--- application/views/partial/registrasi.php
+++ application/views/partial/registrasi.php
@@ -1,5 +1,5 @@
 <!-- section Register -->
-<section style="background: url(<?php echo base_url() ?>/assets/img/bg-slide2.jpg) center top no-repeat; background-size: cover;" class="bar no-mb bg-fixed relative-positioned">
+<section style="background: url(https://www.gad.co.id/assets/img/bg-slide2.jpg) center top no-repeat; background-size: cover;" class="bar no-mb bg-fixed relative-positioned">
     <div class="container zoome boxshdregister bg-white">
         
         <div class="heading text-center">
