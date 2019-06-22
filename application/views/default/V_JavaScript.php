23784d3276249884116de6e5de429d8ecd763a7a
ravhi <ravhi.rizaldi@gad.co.id>
2019-06-22 13:28:33 +0700

    fix mixed content over https

--- application/views/default/V_JavaScript.php
+++ application/views/default/V_JavaScript.php
@@ -1,18 +1,18 @@
-<script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js')?>"></script>
-<script src="<?php echo base_url('assets/vendor/popper.js/umd/popper.min.js')?>"> </script>
-<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.min.js')?>"></script>
-<script src="<?php echo base_url('assets/vendor/jquery.cookie/jquery.cookie.js')?>"> </script>
-<script src="<?php echo base_url('assets/vendor/waypoints/lib/jquery.waypoints.min.js')?>"> </script>
-<script src="<?php echo base_url('assets/vendor/jquery.counterup/jquery.counterup.min.js')?>"> </script>
-<script src="<?php echo base_url('assets/vendor/owl.carousel/owl.carousel.min.js')?>"></script>
-<script src="<?php echo base_url('assets/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js')?>"></script>
-<script src="<?php echo base_url('assets/js/jquery.parallax-1.1.3.js')?>"></script>
-<script src="<?php echo base_url('assets/vendor/bootstrap-select/js/bootstrap-select.min.js')?>"></script>
-<script src="<?php echo base_url('assets/vendor/jquery.scrollto/jquery.scrollTo.min.js')?>"></script>
-<script src="<?php echo base_url('assets/js/front.js')?>"></script>
-<script src="https://blackrockdigital.github.io/startbootstrap-scrolling-nav/vendor/jquery-easing/jquery.easing.min.js"></script>
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script src="<?php echo base_url('assets/js/gad.js')?>"></script>
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="/assets/vendor/popper.js/umd/popper.min.js"> </script>
+<script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="/assets/vendor/jquery.cookie/jquery.cookie.js"> </script>
+<script src="/assets/vendor/waypoints/lib/jquery.waypoints.min.js"> </script>
+<script src="/assets/vendor/jquery.counterup/jquery.counterup.min.js"> </script>
+<script src="/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="/assets/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"></script>
+<script src="/assets/js/jquery.parallax-1.1.3.js"></script>
+<script src="/assets/vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
+<script src="/assets/vendor/jquery.scrollto/jquery.scrollTo.min.js"></script>
+<script src="/assets/js/front.js"></script>
+<script src="//blackrockdigital.github.io/startbootstrap-scrolling-nav/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="//unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="/assets/js/gad.js"></script>
 
 <script>
   AOS.init();
