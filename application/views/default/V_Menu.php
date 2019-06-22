8727743ec4e741d0177da45a710e2afc779e45e0
ravhi <ravhi.rizaldi@gad.co.id>
2019-06-22 14:13:22 +0700

    fix mixed content url

--- application/views/default/V_Menu.php
+++ application/views/default/V_Menu.php
@@ -1,7 +1,7 @@
 
 <header class="nav-holder make-sticky">
         <div id="navbar" role="navigation" class="navbar navbar-expand-lg">
-          <div class="container"><a href="<?php base_url() ?>" class="navbar-brand home"><img src="<?php echo base_url('assets/img/gad-small.png')?>" width="110" height="60" alt="Universal logo" class="d-none d-md-inline-block"><img src="<?php echo base_url('assets/img/gad-small.png')?>"  width="65" height="40" alt="Universal logo" class="d-inline-block d-md-none"><span class="sr-only">GAD </span></a>
+          <div class="container"><a href="<?php base_url() ?>" class="navbar-brand home"><img src="/assets/img/gad-small.png" width="110" height="60" alt="Universal logo" class="d-none d-md-inline-block"><img src="/assets/img/gad-small.png"  width="65" height="40" alt="Universal logo" class="d-inline-block d-md-none"><span class="sr-only">GAD </span></a>
             <button type="button" data-toggle="collapse" data-target="#navigation" class="navbar-toggler btn-template-outlined"><span class="sr-only">Toggle navigation</span><i class="fa fa-align-justify"></i></button>
             <div id="navigation" class="navbar-collapse collapse">
               <ul class="nav navbar-nav ml-auto">
@@ -15,7 +15,7 @@
                   <ul class="dropdown-menu megamenu">
                     <li>
                       <div class="row">
-                        <div class="col-lg-3"><img src="<?php echo base_url('assets/img/callcenter1.png')?>" alt="" class="img-fluid d-none d-lg-block"></div>
+                        <div class="col-lg-3"><img src="/assets/img/callcenter1.png" alt="" class="img-fluid d-none d-lg-block"></div>
                         <div class="col-lg-3 col-md-6">
                           <h5>Partner</h5>
                           <ul class="list-unstyled mb-3">
